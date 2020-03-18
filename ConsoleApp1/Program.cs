864b7d4d16408eab63dd5b4453ca4f1aeed7c724
EMMANUELLA FATOLU <fatoluemmanuella@gmail.com>
2020-03-18 17:32:55 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int birthyear;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello "+ name);
+            Console.WriteLine("Enter your Birth year: " );
+            birthyear = Int32.Parse(Console.ReadLine());
+            int age;
+            age = 2020 - birthyear;
+            Console.WriteLine("you are "+ age+" years old");
+
+            Console.ReadKey();
+        }
+    }
+}
